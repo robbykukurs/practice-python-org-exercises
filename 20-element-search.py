bb53b8f3a67d9915b235ed793f4503432eac3d8a
rkukurs <rkukurs@purestorage.com>
2019-05-26 21:17:17 +0100

    modified:   20-element-search.py

--- 20-element-search.py
+++ 20-element-search.py
@@ -1,4 +1,14 @@
+import time
+
 ordered_list = []
 for number in range(10):
     ordered_list.append(number)
-print ordered_list
+print(ordered_list)
+
+user_value = input("Now enter your number: ")
+time.sleep(1)
+print("Now we're going to see if your number belongs to the list...")
+time.sleep(1)
+for number2 in ordered_list:
+    if number2 == int(user_value):
+        print("Yes, it's a match!")
