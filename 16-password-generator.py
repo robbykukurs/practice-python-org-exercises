2536509e7683418c3c785a7c2b25ec502bf9adc3
rkukurs <rkukurs@purestorage.com>
2019-06-06 21:48:02 +0100

    final commit

--- 16-password-generator.py
+++ 16-password-generator.py
@@ -6,5 +6,11 @@
 #
 #This is a simple radom password generator
 import random
-a = random.random()
-print(a)
+password = []
+for i in range(10):
+    a = random.choice('abcdefghijklmnopqrstuvwxyz0123456789-$%£@!?><')
+    password.append(a)
+#at this stage the list looks like: ['?', 'l', '3', 'b', '?', '-', '9', 'a', '2']
+#we need to join the items together as per below syntax
+final = "".join(password)
+print(final)
